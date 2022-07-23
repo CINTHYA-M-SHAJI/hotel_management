2f9949ff1b5ef3e167ad433141b0ce006284b2d0
CINTHYA M SHAJI <CINTHYA-M-SHAJI>
2022-07-23 14:45:49 +0530

    index page

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Hotel Management SYSTEM</title>
+</head>
+<style>
+    body{
+        background-image: url("images/diamond-hotel-philippines.jpg");
+        background-repeat:no-repeat;
+        background-size:cover;
+    }
+</style>
+<body>	
+<form align='center'>
+  <h1><center>Hotel Management SYSTEM</center></h1>
+  <h2><center>Book Your Rooms</center></h2>
+  
+  <button formaction="login.php"> Login</a></button>
+  <button formaction="signup.php" >Sign Up</a></button>
+</body>
+</html>
